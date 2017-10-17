2eadd5b3483b7c375faf7d5ed43ab83ff27d41cb
Lenz Weber <mail@lenzw.de>
2017-10-17 20:33:59 +0200

    hostApp should now be working on windows and linux

--- index.js
+++ index.js
@@ -99,15 +99,22 @@ async function listEntries() {
   return new Promise((resolve) => {
 
     try {
-      const pass = spawn('find', [
+      let findArgs = [
         '-L',
         '.',
-        '-not', '-path', '\\*/.\\*',
+        '-not', '-path', '*/.*',
         '-type', 'd',
         '-or',
-        '-not', '-path', '\\*/.\\*',
-        '-type', 'f', '-iname', '\\*.gpg',
-      ], {
+        '-not', '-path', '*/.*',
+        '-type', 'f', '-iname', '*.gpg',
+      ];
+
+      if (process.platform === 'win32') {
+        // windows shell needs additional escaping of wildcards
+        findArgs = findArgs.map(arg => arg.replace(/\*/g, '\\*'));
+      }
+
+      const pass = spawn('find', findArgs, {
         cwd: process.env.PASSWORD_STORE_DIR || `${process.env.HOME || '~'}/.password-store`,
       });
       let stdout = [], stderr = [];
